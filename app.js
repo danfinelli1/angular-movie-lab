d655129991bd00cce80245640e96cc067224a54c
Dan Finelli <dan.finelli@hotmail.com>
2016-11-28 22:15:15 -0800

    challenge 5 complete

---
+++ app.js
@@ -0,0 +1,37 @@
+angular.module('movieApp', [])
+    .controller('MovieCtrl', MovieCtrl);
+
+function MovieCtrl(){
+  var vm = this;
+
+  vm.moviesToWatch = [
+    {
+      'title': 'Zootopia'
+    },
+    {
+      'title': 'Hell or High Water'
+    },
+    {
+      'title': 'The Jungle Book'
+    },
+    {
+      'title': 'Moonlight'
+    },
+    {
+      'title': 'Love & Friendship'
+    },
+    {
+      'title': 'Finding Dory'
+    },
+    {
+      'title': 'Hunt for Wilderpeople'
+    }
+  ];
+  vm.submit = function(movieTitle) {
+
+    console.log('this is being called');
+
+      vm.moviesToWatch.push({title: movieTitle});
+
+  }
+}
